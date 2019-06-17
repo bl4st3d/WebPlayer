0dbc592497bd3319656235999b5f3b7d56e7e8a0
amélie <ameliedonny@gmail.com>
2019-06-17 16:47:48 +0200

    create dumb Header

---
+++ src/Components/organisms/Header/index.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Image } from '../../atoms';
+
+export const Header = () => {
+    return (
+        <header>
+            <Image
+                imageUrl="https://www.logoground.com/uploads6/dv6y2019259702019-02-123908587cool-dino.jpg"
+                alt="cool dino"
+                className=""
+            />
+        </header>
+    );
+};
